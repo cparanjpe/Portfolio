ecb036c40a04d388da42671a5b81d1cff21f3700
Chaitanya Paranjpe <112272151+cparanjpe@users.noreply.github.com>
2023-01-10 12:42:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    reset:true,
+    distance: '60px',
+    duration:2500,
+    delay:400
+});
+ScrollReveal().reveal(".heading, .skillheadline",{delay:400, origin:'left'});
+ScrollReveal().reveal(".desc,.profile",{delay:400, origin:'bottom'});
+ScrollReveal().reveal(".smalldesc",{delay:400, origin:'right'});
+ScrollReveal().reveal(".row1",{delay:600, origin:'left'});
+ScrollReveal().reveal(".row2",{delay:750, origin:'right'});
+ScrollReveal().reveal(".contact-heading, .row3",{delay:400, origin:'left'});
+ScrollReveal().reveal(".btn",{delay:900, origin:'right'});
+ScrollReveal().reveal(".contact-me",{delay:300, origin:'bottom'});
